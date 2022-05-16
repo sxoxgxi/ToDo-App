01279df791b0d3c8b2f898a0a9cb9a628195dd2f
SOGI <82437025+sxoxgxi@users.noreply.github.com>
2022-05-16 19:51:53 +0545

    normal

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j-d)gm!1^d#2+x!%tky&osdo+gc5v29=vvy**584cc^%zg$45f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
